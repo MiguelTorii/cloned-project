08713f12cc5e3ffe2f4a203f970e6b021ca8b331
Camilo Rios <camilorios@Camilos-MacBook-Pro.local>
2019-04-03 13:57:42 -0300

    add check for user token exp date

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,19 +1,31 @@
 // @flow
 import axios from 'axios';
 import store from 'store';
+import decode from 'jwt-decode';
+import moment from 'moment';
 import { API_ROUTES } from '../constants/routes';
 
 export const getToken = async (): Promise<string> => {
-  const token = store.get('REFRESH_TOKEN');
+  const token = store.get('TOKEN');
+  const refreshToken = store.get('REFRESH_TOKEN');
   const userId = store.get('USER_ID');
   const segment = store.get('SEGMENT');
-  if (segment === '' || userId === '' || token === '' || !userId) {
+
+  if (token) {
+    const decoded = decode(token);
+    const { exp } = decoded;
+    const date = moment().subtract(2, 'minutes');
+    if (exp > date.format('X')) {
+      return token;
+    }
+  }
+  if (segment === '' || userId === '' || refreshToken === '' || !userId) {
     return '';
   }
 
   const result = await axios.post(API_ROUTES.REFRESH, {
     user_id: Number(userId),
-    token,
+    token: refreshToken,
     segment
   });
   const { data = {} } = result;
