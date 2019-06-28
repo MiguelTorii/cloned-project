674b61e61fe6caed4ee7bd00693aab5b02de77d0
camiloriosfig <camilo.rios@toptal.com>
2019-06-28 14:29:13 -0400

    fix twilio error

--- src/containers/FloatingChat/index.js
+++ src/containers/FloatingChat/index.js
@@ -1,19 +1,36 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import withWidth from '@material-ui/core/withWidth';
+import type { State as StoreState } from '../../types/state';
+import type { UserState } from '../../reducers/user';
 import FloatingChat from './FloatingChat';
 import ErrorBoundary from '../ErrorBoundary';
 
 type Props = {
-  width: string
+  width: string,
+  user: UserState
 };
 
 class Chat extends React.PureComponent<Props> {
   render() {
-    const { width } = this.props;
+    const {
+      user: {
+        data: { userId }
+      },
+      width
+    } = this.props;
+    if (userId === '') return null;
     return <ErrorBoundary>{width !== 'xs' && <FloatingChat />}</ErrorBoundary>;
   }
 }
 
-export default withWidth()(Chat);
+const mapStateToProps = ({ user }: StoreState): {} => ({
+  user
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(withWidth()(Chat));
