0e7d15530f6d19d2ae75a4347a09f975854d3f32
Felipe Machado <felipe.machado@toptal.com>
2020-08-21 16:37:30 +0000

    Merged in new-saml-school (pull request #426)
    Adding new school
    
    Signed-off-by: Felipe Machado <felipe.machado@toptal.com>

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -64,7 +64,7 @@ const Auth = ({ classes, pushTo, updateSchool }: Props) => {
       pushTo('/login')
     } else if (lmsTypeId === -1) {
       window.location.replace('https://circleinapp.com/whitelist');
-    } else if (value.id === 55) {
+    } else if (value.id === 57) {
       const webAuth = new auth0.WebAuth({
         domain:       'circlein-dev.us.auth0.com',
         clientID:     'Bps2iaRf3iIxDeTVJa9zOQI20937s7Dj'
@@ -73,6 +73,17 @@ const Auth = ({ classes, pushTo, updateSchool }: Props) => {
         audience: 'https://circlein-dev.us.auth0.com/api/v2/',
         redirectUri: `${window.location.origin}/saml`,
         connection: 'Test-Columbia-College',
+        responseType: 'token'
+      })
+    } else if (value.id === 55) {
+      const webAuth = new auth0.WebAuth({
+        domain:       'circlein-dev.us.auth0.com',
+        clientID:     'Bps2iaRf3iIxDeTVJa9zOQI20937s7Dj'
+      });
+      webAuth.authorize({
+        audience: 'https://circlein-dev.us.auth0.com/api/v2/',
+        redirectUri: `${window.location.origin}/saml`,
+        connection: 'Lamar-State-College-Test2',
         responseType: 'token'
       })
     } else {
