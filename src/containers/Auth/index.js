0a8736674e82997a10fa095169c94c7a1669120c
Jon Tseng <jt9193@gmail.com>
2020-08-25 08:37:39 -0400

    Comment out use of auth0 library

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -7,7 +7,7 @@ import { push } from 'connected-react-router';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import auth0 from 'auth0-js'
+//import auth0 from 'auth0-js'
 import type { State as StoreState } from '../../types/state';
 import ErrorBoundary from '../ErrorBoundary';
 import loginBackground from '../../assets/img/login-background.png';
