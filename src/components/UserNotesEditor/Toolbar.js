d47248b472aab1dbc0524ee89cca791947f47970
Felipe Machado <felipe.machado@toptal.com>
2021-01-27 16:03:27 +0000

    Merged in removing-warning (pull request #546)
    removing warning

--- src/components/UserNotesEditor/Toolbar.js
+++ src/components/UserNotesEditor/Toolbar.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from "react";
+import React from "react";
 import cx from 'classnames'
 import ReactDOMServer from 'react-dom/server';
 import { Quill } from "react-quill";
