7268917ba700458944528d2ba401cbbdab8d4277
Dang Nguyen <dtn1712.work@gmail.com>
2021-01-29 14:37:58 +0000

    Merged in feature/student_jobs (pull request #548)
    Fix cors error
    
    * fix: pr review
    
    * Merge branch 'master' of bitbucket.org:MyQVO/web-app-frontend-v2 into feature/student_jobs
    
    * update student job pdf url
    
    * update pdf url from http to https
    
    * fix: merge conflict
    
    * Merge branch 'master' of bitbucket.org:MyQVO/web-app-frontend-v2 into feature/student_jobs
    
    * choice: add log (pdf render)
    
    * update react-pdf version, change pdf workerSrc value to cdn link
    
    * Merge branch 'master' of bitbucket.org:MyQVO/web-app-frontend-v2 into feature/student_jobs
    
    * Fix: react-pdf cors error (add pdf file to project assets)
    
    Approved-by: Felipe Machado

--- src/components/GetStudentJob/index.js
+++ src/components/GetStudentJob/index.js
@@ -3,10 +3,9 @@ import React from 'react'
 import Dialog from 'components/Dialog'
 import { pdfjs } from 'react-pdf';
 import PdfComponent from 'components/PdfGallery/PdfComponent'
+import studentJobPdf from 'assets/pdf/CircleIn-Student-Ambassador.pdf'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-const STUDENT_JOB_PDF_URI = "https://media.circleinapp.com/CircleIn+Student+Ambassador.pdf"
 
 type Props = {
   open: boolean, onClose: Function
@@ -20,7 +19,7 @@ const GetAppDialog = ({ open, onClose }: Props) => {
       open={Boolean(open)}
     >
       <PdfComponent
-        url={STUDENT_JOB_PDF_URI}
+        url={studentJobPdf}
         height="100%"
         width="100%"
         radius={10}
