7e1af1a9b9a7c20b0509abc3d0384b71c7900328
Felipe Machado <felipe.machado@toptal.com>
2021-03-01 20:31:27 +0000

    Merged in changing-latex-url (pull request #592)
    changing latex url
    
    * changing latex url
    
    Signed-off-by: Felipe Machado <felipe.machado@toptal.com>

--- src/utils/quill.js
+++ src/utils/quill.js
@@ -2,6 +2,6 @@ export default defaultColor => {
   window.katex = {}
   window.katex.render = (value, node, { color, dpi }) => {
     // eslint-disable-next-line
-    node.innerHTML = `<img src='https://private.codecogs.com/png.download?\\dpi{${dpi || 400}}\\color{${color || defaultColor}}${value}' />`
+    node.innerHTML = `<img src='https://private.codecogs.com/png.latex?\\dpi{${dpi || 400}}\\color{${color || defaultColor}}${value}' />`
   }
 }
