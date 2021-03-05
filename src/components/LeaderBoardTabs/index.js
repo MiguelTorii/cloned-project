f8465cb5328528c6b31301b7f8d3b32c778570f9
Dang Nguyen <dtn1712.work@gmail.com>
2021-03-05 20:28:45 +0000

    Merged in feature/WF-48 (pull request #607)
    update text of Leaderboards
    
    * update text of Leaderboards

--- src/components/LeaderBoardTabs/index.js
+++ src/components/LeaderBoardTabs/index.js
@@ -308,7 +308,7 @@ const LeaderBoardTabs = ({
                   </Button>
                 </div>
               </div>
-              <div style={{ marginLeft: 16}}>
+              <div style={{ marginLeft: 16 }}>
                 <div style={{ marginBottom: 2 }}>
                   <Typography variant="h5">{text}</Typography>
                 </div>
@@ -348,7 +348,7 @@ const LeaderBoardTabs = ({
             <span className={classes.count}>{tuesdayPoints}</span>
           </div>
           <div className={classes.footnote}>
-            *You must earn a minimum of 200,000 points per class on CircleIn to qualify for Monthly Rewards
+            *You must earn a minimum of 200,000 points on CircleIn to qualify for Monthly Rewards
           </div>
         </div>
         <div className={classes.rewardContainer}>
