f32a582e851d3b2586e10e82d587a687c726c3e8
Kei Mori <devninjamori@gmail.com>
2021-04-19 15:02:02 +0000

    Merged in feature/redux_devtool (pull request #675)
    Add Redux Devtool
    
    * Add Redux Devtool
    
    Approved-by: Quamain Clark

--- src/configureStore.js
+++ src/configureStore.js
@@ -5,12 +5,17 @@ import thunk from 'redux-thunk';
 import createRootReducer from './reducers';
 
 export const history = createBrowserHistory();
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 export default function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history),
     preloadedState,
-    compose(applyMiddleware(routerMiddleware(history), thunk))
+    composeEnhancers(
+      applyMiddleware(
+        routerMiddleware(history), thunk
+      )
+    )
   );
 
   return store;
