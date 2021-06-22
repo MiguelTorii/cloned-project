c2c24f4230b41ca484bf7e2b159897cf03acb213
Kei Mori <devninjamori@gmail.com>
2021-06-22 15:15:00 +0000

    Merged in bug/BNE-103 (pull request #871)
    Bug/BNE-103
    
    * Update time difference
    
    * Merge branch 'development' into bug/BNE-103
    
    * Merge branch 'development' into bug/BNE-103
    
    * Remove warning
    
    Approved-by: Dang Nguyen

--- src/components/PointsRecordItem/index.js
+++ src/components/PointsRecordItem/index.js
@@ -5,7 +5,6 @@ import Paper from '@material-ui/core/Paper';
 import { Box } from '@material-ui/core';
 import LoadImg from '../LoadImg';
 import Typography from '@material-ui/core/Typography';
-import { momentWithTimezone } from '../../utils/helpers';
 import moment from 'moment';
 
 import { useStyles } from '../_styles/PointsRecordItem';
