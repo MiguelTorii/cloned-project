e99a9b58c1da18c56525c87975d0c8e80f162ed4
Edwin Rosembert <edwinjimmy417@gmail.com>
2021-06-24 14:45:33 +0000

    Merged in bugfix/NCGNT-113 (pull request #883)
    Remove my classes tooltip
    
    * Remove my classes tooltip
    
    * remove my classes tooltip
    
    Approved-by: Kei Mori

--- src/components/MainLayout/Drawer.js
+++ src/components/MainLayout/Drawer.js
@@ -333,26 +333,6 @@ const Drawer = ({
           // landingPageCampaign={landingPageCampaign}
           // openClassmatesDialog={openClassmatesDialog('classmate')}
         />}
-        {!expertMode && (
-          <Tooltip
-            id={9054}
-            placement="right"
-            okButton='Okay!'
-            text="Hey! You can access your classes by clicking “My Classes”"
-          >
-            <DrawerItem
-              OnIcon={<MyClassOn />}
-              primaryText='My Classes'
-              pathname={pathname}
-              component={MyLink}
-              link="/classes"
-              OffIcon={<MyClassOff />}
-              listItemClass={classNames(
-                ['/classes'].includes(pathname) ? classes.currentPath : classes.otherPath
-              )}
-            />
-          </Tooltip>
-        )}
         {!landingPageCampaign && (
           <DrawerItem
             OnIcon={<WorkflowIconOn />}
