2e8be01ea9012c4fb680774693ccb79f6a7340af
Dang Nguyen <dev.danguyen1712@gmail.com>
2021-06-25 14:50:01 +0000

    Merged in feature/NCGNT-240 (pull request #891)
    remove: the usage of the user data from the chat API
    
    * remove: the usage of the user data from the chat API
    
    Approved-by: Kei Mori
    Approved-by: Edwin Rosembert

--- src/api/chat.js
+++ src/api/chat.js
@@ -264,16 +264,7 @@ export const getChannels = async (): Promise<Object> => {
             image: c.last_received_message.user.profile_image_url,
           },
         },
-        thumbnail: c.thumbnail,
-        members: c.users.map(u => ({
-          firstname: u.first_name,
-          lastname: u.last_name,
-          userId: u.user_id,
-          image: u.profile_image_url,
-          role: u.role,
-          roleId: u.role_id,
-          isOnline: u.is_online,
-        }))
+        thumbnail: c.thumbnail
       }
     });
     return local
--- src/components/CommunityChatListItem/index.js
+++ src/components/CommunityChatListItem/index.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import MainChatItem from 'components/CommunityChatListItem/MainChatItem';
 import ErrorBoundary from 'containers/ErrorBoundary';
+import { getGroupMembers } from 'api/chat'
 import clsx from 'clsx'
 
 type Props = {
@@ -38,22 +39,29 @@ const ChatListItem = ({
   const [thumbnail, setThumbnail] = useState('')
   const [isOnline, setIsOnline] = useState(false)
   const [isDirectChat, setIsDirectChat] = useState(false)
+  const [members, setMembers] = useState([])
 
   useEffect(() => {
-    if(channel && channel.members) {
-      if(channel.members.length === 2) {
-        setIsDirectChat(true)
-        channel.members.forEach(member => {
-          if (Number(member.userId) !== Number(userId)) {
-            setName(`${member.firstname} ${member.lastname}`)
-            setThumbnail(member.image)
-            setIsOnline(member.isOnline)
-          }
-        })
-      } else {
-        setThumbnail(channel.thumbnail)
+    const getMembers = async () => {
+      const members = await getGroupMembers({ chatId: channel.sid })
+      setMembers(members)
+      if(channel && members) {
+        if(members.length === 2) {
+          setIsDirectChat(true)
+          members.forEach(member => {
+            if (Number(member.userId) !== Number(userId)) {
+              setName(`${member.firstname} ${member.lastname}`)
+              setThumbnail(member.image)
+              setIsOnline(member.isOnline)
+            }
+          })
+        } else {
+          setThumbnail(channel.thumbnail)
+        }
       }
     }
+
+    getMembers()
   }, [channel, userId])
 
   const handleOpenChannel = () => {
@@ -79,7 +87,7 @@ const ChatListItem = ({
         selected={selected}
         name={name}
         muted={channel.muted}
-        members={channel.members}
+        members={members}
         roomName={channel.title}
         lastMessage={clsx(channel.lastMessage && channel.lastMessage.message)}
         unReadCount={channel.unread}
