03c71b12249f6339a9ed09677a3ae4db1a251b42
dtn1712 <dev.danguyen1712@gmail.com>
2021-09-23 12:26:10 -0700

    fix: unread message count issue

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -520,11 +520,9 @@ export const handleInitChat =
         let unreadCount;
         if (channel.lastConsumedMessageIndex === null) {
           unreadCount = count;
-        } else if (
-          channel.lastConsumedMessageIndex === 0 ||
-          channel.lastConsumedMessageIndex - 1 > count) {
-          // check the channel is new or check lastConsumedMessageIndex is bigger than message count
+        } else if (channel.lastConsumedMessageIndex + 1 > count) {
           unreadCount = 0;
+          // check the channel is new or check lastConsumedMessageIndex is bigger than message count
         } else {
           unreadCount = count - (channel.lastConsumedMessageIndex + 1);
         }
