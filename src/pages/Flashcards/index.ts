2e3f49f6a0c4bf603cdf48b2d4a85f3822a3aeaa
Kori Ryter <kryter@gmail.com>
2021-10-26 17:04:55 -0500

    Remove unnecessary index file with indirection.
    Uncovered the fact that FlashcardsNewPage was not used so deleted it.

--- src/pages/Flashcards/index.ts
+++
@@ -1,6 +0,0 @@
-import FlashcardsListPage from './FlashcardsListPage';
-import FlashcardsNewPage from './FlashcardsNewPage';
-import FlashcardsShowPage from './FlashcardsShowPage';
-import FlashcardsEditPage from './FlashcardsEditPage';
-
-export { FlashcardsListPage, FlashcardsNewPage, FlashcardsShowPage, FlashcardsEditPage };
