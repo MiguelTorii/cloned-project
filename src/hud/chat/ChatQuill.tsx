8be5848c270a9a06a80ebb1d472d3929069d9b98
Kori Ryter <kryter@gmail.com>
2021-11-15 10:41:37 -0600

    remove commented out code

--- src/hud/chat/ChatQuill.tsx
+++ src/hud/chat/ChatQuill.tsx
@@ -19,31 +19,7 @@ const ChatQuill = () => {
     (state: { hudChat: HudChatState }) => state.hudChat.idToChannel[selectedChannelId]
   );
 
-  return (
-    <>
-      {/* <MessageQuill
-        isCommunityChat={isCommunityChat}
-        value={value}
-        userId={userId}
-        setFiles={setFiles}
-        files={files}
-        focusMessageBox={focusMessageBox}
-        onSendMessage={onSendMessage}
-        onChange={handleRTEChange}
-        showNotification={showNotification}
-        setValue={setValue}
-        handleClick={handleClick}
-        onTyping={onTyping}
-        showError={showError}
-      />
-
-      <div className={classes.typing}>
-        <Typography className={classes.typingText} variant="subtitle1">
-          {typing && typing.channel === channel.sid ? `${typing.friendlyName} is typing ...` : ''}
-        </Typography>
-      </div> */}
-    </>
-  );
+  return <div>{`Chat quill for selected channel ${selectedChannelId} goes here.`}</div>;
 };
 
 export default ChatQuill;
