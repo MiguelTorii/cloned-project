5ab00eff6f76e6137d593a8e1cda7c8f023eafe2
Jaron Johnson <jaron@circleinapp.com>
2021-11-16 13:17:08 -0700

    removed extra files hanging around

--- src/hud/experienceBarState/hudExperienceBarState.ts
+++
@@ -1,9 +0,0 @@
-export type ExperienceBarState = {
-  experienceBarPoints: number;
-  experienceBarTotal: number;
-};
-
-export const defaultState: ExperienceBarState = {
-  experienceBarPoints: 3500,
-  experienceBarTotal: 5000
-};
