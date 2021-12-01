8ea334861de183cab88264af33813bd395d7aea3
Jaron Johnson <jaron@circleinapp.com>
2021-12-01 08:45:04 -0700

    Add CircleIn logo, move signout button to profile

--- src/hud/storyState/useStorySequence.ts
+++ src/hud/storyState/useStorySequence.ts
@@ -6,20 +6,26 @@ import { HudStoryState } from './hudStoryState';
 
 // story sequence delay in milliseconds
 const storySequenceDelay = 7000;
+let storyTimeout;
 
-const storyTimeout = (storyMessage, index) => {
-  const dispatch = useDispatch();
-  setTimeout(() => {
-    dispatch(setConversation(storyMessage));
-  }, storySequenceDelay * index);
-};
+// todo figure out a cleartimeout to start over fresh again
+// const StoryTimeout = (storyMessage, index) => {
+//   const dispatch = useDispatch();
+//   setTimeout(() => {
+//     dispatch(setConversation(storyMessage));
+//   }, storySequenceDelay * index);
+// };
 
 const useStorySequence = () => {
   const dispatch = useDispatch();
   const startConversation = (storySequence: string[]) => {
     const storySequenceWithEndMarker = [...storySequence, ''];
     storySequenceWithEndMarker.forEach((storyMessage, index) => {
-      setTimeout(() => {
+      if (!initialLoadTriggered) {
+        clearTimeout(storyTimeout);
+        dispatch(setInitialLoad());
+      }
+      storyTimeout = setTimeout(() => {
         dispatch(setConversation(storyMessage));
       }, storySequenceDelay * index);
     });
@@ -34,7 +40,6 @@ const useStorySequence = () => {
       fetchGreetings(moment().format('YYYY-MM-DDThh:mm:ss')).then((welcomeMessage) => {
         const storySequence = [welcomeMessage.greetings.title, welcomeMessage.greetings.body];
         startConversation(storySequence);
-        dispatch(setInitialLoad());
       });
     }
   };
