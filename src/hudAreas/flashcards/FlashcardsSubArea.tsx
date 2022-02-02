8b20e127142cc0194f41585c0005e28b99dfc6bc
Kei Mori <74977916+kmoripersonal@users.noreply.github.com>
2022-02-02 12:37:26 -0800

    Edit Flashcards from ClassFeeds (#417)

--- src/hudAreas/flashcards/FlashcardsSubArea.tsx
+++ src/hudAreas/flashcards/FlashcardsSubArea.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 import FlashcardsList from '../../containers/FlashcardsList/FlashcardsList';
 import FlashcardsShow from '../../containers/FlashcardsShow/FlashcardsShow';
+import FlashcardsEdit from 'containers/FlashcardsEdit/FlashcardsEdit';
 
 const FlashcardsSubArea = () => {
   const { flashcardId } = useParams();
+  const { pathname } = useLocation();
+
+  if (pathname.startsWith('/edit/flashcards')) {
+    return <FlashcardsEdit flashcardId={flashcardId} />;
+  }
 
   if (flashcardId) {
     return <FlashcardsShow />;
